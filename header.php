b72448b2cd5fa9d3ff5bedde199c63244e89a512
luis colca <luis.7colca@gmail.com>
2019-11-28 10:16:46 -0500

    mi primer cambioo

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<script src='js/jquery.js' type='text/javascript'></script>
+		<script src='js/bootstrap.min.js' type='text/javascript'></script>
+		<link href='css/css.css' rel='stylesheet' type='text/css'>
+		<link href='css/bootstrap.min.css' rel='stylesheet' type='text/css'>
+		<link href='css/bootstrap-theme.min.css' rel='stylesheet' type='text/css'>
+		
+		<title>Tienda zapato Online</title>
+	</head>
+	
+	<body>
+	<header>
+		<img src='' id='cab'>
+		<h1><p class="text-center">TIENDA DE CALZADOS</p></h1>
+		
+			
+		<nav class="navbar navbar-inverse" role="navigation" id="supmenu">
+			<ul class="nav nav-pills">
+				<li class="active"><a href='index.php'>Inicio</a></li>
+				<li><a href='productos.php'>Productos</a></li>
+				<li class="dropdown" role="presentation"><a lass="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Administrador</a>
+					<ul class="dropdown-menu">
+						<li><a href='registraprod.php'>RegistrarProducto</a></li>
+						<li><a href='eliminaprod.php'>EliminaProducto</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+		<section>
+		<img src="imagenes/zapato1.jpg">
+		<img src="imagenes/zapato2.jpg">
+		<img src="imagenes/zapato3.jpg">
+		
+			</section>
+	</header>
+	
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include 'header.php'; ?>
+	<article>
+			<h1><p class="text-center">BIENVENIDOS</p> </h1>
+	</article>
+<?php include 'footer.php'; ?>
+
